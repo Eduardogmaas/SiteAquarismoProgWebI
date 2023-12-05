cb83dd1c70ff9610a53f382c3040959a5c135501
Eduardogmaas <eduardogmaas@gmail.com>
2023-12-05 17:08:54 -0300

    d

---
+++ site/js/doacao.js
@@ -0,0 +1,19 @@
+let totalArrecadado = 0;
+let metodoPagamento = 'dinheiro';
+
+function formatarValor(valor) {
+    return valor.toFixed(2);
+}
+
+function doar() {
+    const valorDigitado = document.getElementById('valor').value;
+    const valor = parseFloat(valorDigitado) || 0;
+    totalArrecadado += valor;
+    document.getElementById('totalArrecadado').textContent = totalArrecadado;
+
+    document.getElementById('valor').value = '';
+}
+
+function atualizarMetodo() {
+    metodoPagamento = document.getElementById('metodo').value;
+}
